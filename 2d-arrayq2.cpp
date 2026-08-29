611bc6c33e9f8de970731068857f489b3a109634
shanum-123 <shanumsayyad@gmail.com>
2026-08-30 01:45:20 +0530

    Add files via upload

---
+++ 2d-arrayq2.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a[20][20],i,j,sum=0;
+	printf("enter the matrix:");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			sum=sum+a[i][j];
+		}
+	}
+	printf("sum of matrix=%d",sum);
+}
