5340cbc9ed22f9c07e4a0c5c7863a40dbff52e8c
shanum-123 <shanumsayyad@gmail.com>
2026-08-30 01:49:14 +0530

    Add files via upload

---
+++ 2d-arrayq4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a[20][20],i,j,sum=0;
+	printf("enter the matrix:");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			if(i<=j)
+			{
+				sum=sum+a[i][j];
+			}
+		}
+	}
+	printf("sum of upper triangular matrix=%d",sum);
+}
